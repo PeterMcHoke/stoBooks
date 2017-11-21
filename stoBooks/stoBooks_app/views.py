73fbf37e3d94a07e6efbd8c1425dd970f0f31230
alexsocheathson <alexson28@gmail.com>
2017-11-20 18:04:22 -0600

    Moved single index page onto Django framework

---
+++ stoBooks/stoBooks_app/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader #allows django to import views.
+
+# Create your views here.
+def index(request):
+    return render(request, 'stoBooks_app/index.html', {})
