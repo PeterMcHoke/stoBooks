0a1271c9f8bb638e8d9d8349c17a50da56d16837
PeterMcHoke <petermchoke@gmail.com>
2017-11-23 14:51:48 -0600

    Making table searchable and creating a pseudo sell function (not functional)

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,141 +1,141 @@
-jQuery(document).ready(function($){
-	var $form_modal = $('.cd-user-modal'),
-		$form_login = $form_modal.find('#cd-login'),
-		$form_signup = $form_modal.find('#cd-signup'),
-		$form_forgot_password = $form_modal.find('#cd-reset-password'),
-		$form_modal_tab = $('.cd-switcher'),
-		$tab_login = $form_modal_tab.children('li').eq(0).children('a'),
-		$tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
-		$forgot_password_link = $form_login.find('.cd-form-bottom-message a'),
-		$back_to_login_link = $form_forgot_password.find('.cd-form-bottom-message a'),
-		$main_nav = $('.main-nav');
-
-	//open modal
-	$main_nav.on('click', function(event){
-
-		if( $(event.target).is($main_nav) ) {
-			// on mobile open the submenu
-			$(this).children('ul').toggleClass('is-visible');
-		} else {
-			// on mobile close submenu
-			$main_nav.children('ul').removeClass('is-visible');
-			//show modal layer
-			$form_modal.addClass('is-visible');
-			//show the selected form
-			( $(event.target).is('.cd-signup') ) ? signup_selected() : login_selected();
-		}
-
-	});
-
-	//close modal
-	$('.cd-user-modal').on('click', function(event){
-		if( $(event.target).is($form_modal) || $(event.target).is('.cd-close-form') ) {
-			$form_modal.removeClass('is-visible');
-		}
-	});
-	//close modal when clicking the esc keyboard button
-	$(document).keyup(function(event){
-    	if(event.which=='27'){
-    		$form_modal.removeClass('is-visible');
-	    }
-    });
-
-	//switch from a tab to another
-	$form_modal_tab.on('click', function(event) {
-		event.preventDefault();
-		( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
-	});
-
-	//hide or show password
-	$('.hide-password').on('click', function(){
-		var $this= $(this),
-			$password_field = $this.prev('input');
-
-		( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
-		( 'Hide' == $this.text() ) ? $this.text('Show') : $this.text('Hide');
-		//focus and move cursor to the end of input field
-		$password_field.putCursorAtEnd();
-	});
-
-	//show forgot-password form
-	$forgot_password_link.on('click', function(event){
-		event.preventDefault();
-		forgot_password_selected();
-	});
-
-	//back to login from the forgot-password form
-	$back_to_login_link.on('click', function(event){
-		event.preventDefault();
-		login_selected();
-	});
-
-	function login_selected(){
-		$form_login.addClass('is-selected');
-		$form_signup.removeClass('is-selected');
-		$form_forgot_password.removeClass('is-selected');
-		$tab_login.addClass('selected');
-		$tab_signup.removeClass('selected');
-	}
-
-	function signup_selected(){
-		$form_login.removeClass('is-selected');
-		$form_signup.addClass('is-selected');
-		$form_forgot_password.removeClass('is-selected');
-		$tab_login.removeClass('selected');
-		$tab_signup.addClass('selected');
-	}
-
-	function forgot_password_selected(){
-		$form_login.removeClass('is-selected');
-		$form_signup.removeClass('is-selected');
-		$form_forgot_password.addClass('is-selected');
-	}
-
-
-
-	//IE9 placeholder fallback
-	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
-	if(!Modernizr.input.placeholder){
-		$('[placeholder]').focus(function() {
-			var input = $(this);
-			if (input.val() == input.attr('placeholder')) {
-				input.val('');
-		  	}
-		}).blur(function() {
-		 	var input = $(this);
-		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
-				input.val(input.attr('placeholder'));
-		  	}
-		}).blur();
-		$('[placeholder]').parents('form').submit(function() {
-		  	$(this).find('[placeholder]').each(function() {
-				var input = $(this);
-				if (input.val() == input.attr('placeholder')) {
-			 		input.val('');
-				}
-		  	})
-		});
-	}
-
-});
-
-
-//credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
-jQuery.fn.putCursorAtEnd = function() {
-	return this.each(function() {
-    	// If this function exists...
-    	if (this.setSelectionRange) {
-      		// ... then use it (Doesn't work in IE)
-      		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-      		var len = $(this).val().length * 2;
-      		this.setSelectionRange(len, len);
-    	} else {
-    		// ... otherwise replace the contents with itself
-    		// (Doesn't work in Google Chrome)
-      		$(this).val($(this).val());
-    	}
-	});
-};
+// jQuery(document).ready(function($){
+// 	var $form_modal = $('.cd-user-modal'),
+// 		$form_login = $form_modal.find('#cd-login'),
+// 		$form_signup = $form_modal.find('#cd-signup'),
+// 		$form_forgot_password = $form_modal.find('#cd-reset-password'),
+// 		$form_modal_tab = $('.cd-switcher'),
+// 		$tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+// 		$tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+// 		$forgot_password_link = $form_login.find('.cd-form-bottom-message a'),
+// 		$back_to_login_link = $form_forgot_password.find('.cd-form-bottom-message a'),
+// 		$main_nav = $('.main-nav');
+//
+// 	//open modal
+// 	$main_nav.on('click', function(event){
+//
+// 		if( $(event.target).is($main_nav) ) {
+// 			// on mobile open the submenu
+// 			$(this).children('ul').toggleClass('is-visible');
+// 		} else {
+// 			// on mobile close submenu
+// 			$main_nav.children('ul').removeClass('is-visible');
+// 			//show modal layer
+// 			$form_modal.addClass('is-visible');
+// 			//show the selected form
+// 			( $(event.target).is('.cd-signup') ) ? signup_selected() : login_selected();
+// 		}
+//
+// 	});
+//
+// 	//close modal
+// 	$('.cd-user-modal').on('click', function(event){
+// 		if( $(event.target).is($form_modal) || $(event.target).is('.cd-close-form') ) {
+// 			$form_modal.removeClass('is-visible');
+// 		}
+// 	});
+// 	//close modal when clicking the esc keyboard button
+// 	$(document).keyup(function(event){
+//     	if(event.which=='27'){
+//     		$form_modal.removeClass('is-visible');
+// 	    }
+//     });
+//
+// 	//switch from a tab to another
+// 	$form_modal_tab.on('click', function(event) {
+// 		event.preventDefault();
+// 		( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
+// 	});
+//
+// 	//hide or show password
+// 	$('.hide-password').on('click', function(){
+// 		var $this= $(this),
+// 			$password_field = $this.prev('input');
+//
+// 		( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+// 		( 'Hide' == $this.text() ) ? $this.text('Show') : $this.text('Hide');
+// 		//focus and move cursor to the end of input field
+// 		$password_field.putCursorAtEnd();
+// 	});
+//
+// 	//show forgot-password form
+// 	$forgot_password_link.on('click', function(event){
+// 		event.preventDefault();
+// 		forgot_password_selected();
+// 	});
+//
+// 	//back to login from the forgot-password form
+// 	$back_to_login_link.on('click', function(event){
+// 		event.preventDefault();
+// 		login_selected();
+// 	});
+//
+// 	function login_selected(){
+// 		$form_login.addClass('is-selected');
+// 		$form_signup.removeClass('is-selected');
+// 		$form_forgot_password.removeClass('is-selected');
+// 		$tab_login.addClass('selected');
+// 		$tab_signup.removeClass('selected');
+// 	}
+//
+// 	function signup_selected(){
+// 		$form_login.removeClass('is-selected');
+// 		$form_signup.addClass('is-selected');
+// 		$form_forgot_password.removeClass('is-selected');
+// 		$tab_login.removeClass('selected');
+// 		$tab_signup.addClass('selected');
+// 	}
+//
+// 	function forgot_password_selected(){
+// 		$form_login.removeClass('is-selected');
+// 		$form_signup.removeClass('is-selected');
+// 		$form_forgot_password.addClass('is-selected');
+// 	}
+//
+//
+//
+// 	//IE9 placeholder fallback
+// 	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+// 	if(!Modernizr.input.placeholder){
+// 		$('[placeholder]').focus(function() {
+// 			var input = $(this);
+// 			if (input.val() == input.attr('placeholder')) {
+// 				input.val('');
+// 		  	}
+// 		}).blur(function() {
+// 		 	var input = $(this);
+// 		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+// 				input.val(input.attr('placeholder'));
+// 		  	}
+// 		}).blur();
+// 		$('[placeholder]').parents('form').submit(function() {
+// 		  	$(this).find('[placeholder]').each(function() {
+// 				var input = $(this);
+// 				if (input.val() == input.attr('placeholder')) {
+// 			 		input.val('');
+// 				}
+// 		  	})
+// 		});
+// 	}
+//
+// });
+//
+//
+// //credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+// jQuery.fn.putCursorAtEnd = function() {
+// 	return this.each(function() {
+//     	// If this function exists...
+//     	if (this.setSelectionRange) {
+//       		// ... then use it (Doesn't work in IE)
+//       		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+//       		var len = $(this).val().length * 2;
+//       		this.setSelectionRange(len, len);
+//     	} else {
+//     		// ... otherwise replace the contents with itself
+//     		// (Doesn't work in Google Chrome)
+//       		$(this).val($(this).val());
+//     	}
+// 	});
+// };
 
 (function($) {
   "use strict"; // Start of use strict
@@ -179,3 +179,52 @@ jQuery.fn.putCursorAtEnd = function() {
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+function searchBooks() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("stoSearch");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("stoLibrary");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+//Rework this function to use arrays (inefficient af rn)
+function sellBooks() {
+	var table, row, title, author, edition, dept, number, isbn, price;
+//Parsing the values from the input fields
+	table = document.getElementById("stoLibrary");
+	row = table.insertRow(0);
+	title = document.getElementById("inputTitle").value;
+  author = document.getElementById("inputAuthor").value;
+	edition = document.getElementById("inputEdition").value;
+	dept = document.getElementById("inputDept").value;
+	number = document.getElementById("inputClass").value;
+	isbn = document.getElementById("inputISBN").value;
+	price = document.getElementById("inputPrice").value;
+//Highlighting the columns (cells) to insert info from input
+	var titleCol, authorCol, editionCol, deptCol, numberCol, isbnCol, priceCol;
+	titleCol = row.insertCell(0);
+	authorCol = row.insertCell(1);
+	editionCol = row.insertCell(2);
+	deptCol = row.insertCell(3);
+	numberCol = row.insertCell(4);
+	isbnCol = row.insertCell(5);
+	priceCol = row.insertCell(6);
+//Adding a row with all of the info from the input fields
+	titleCol.innerHTML = title;
+	authorCol.innerHTML = author;
+	editionCol.innerHTML = edition;
+	deptCol.innerHTML = dept;
+	numberCol.innerHTML = number;
+	isbnCol.innerHTML = isbn;
+	priceCol.innerHTML = price;
+}
