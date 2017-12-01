cd4526ff02e7d2f02b2f6f9d476a9b11c9428779
PeterMcHoke <petermchoke@gmail.com>
2017-12-01 16:33:28 -0600

    First push to models with modified model field type

--- stoBooks/stoBooks_app/views.py
+++ stoBooks/stoBooks_app/views.py
@@ -3,8 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader #allows django to import views.
+from django.template import loader #allows django to import vi
 
 # Create your views here.
 def index(request):
     return render(request, 'stoBooks_app/index.html', {})
+
+
+
+
+
